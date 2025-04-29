c8de8fe1936f660b35497539b36c7e66618e073d
Pranay Reddy <pannalapranay1406@gmail.com>
2025-04-29 10:51:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,25 +12,25 @@ import mongoose from 'mongoose';
 //     );
 // }
 
-// Using hardcoded MongoDB connection string (not recommended for production)
-// export const connectDB = async ()=> {
-//     try {
-//         const MONGO_URI = 'mongodb://127.0.0.1:27017/llcdb'; // Replace with your MongoDB connection string
-//         const conn = await mongoose.connect(MONGO_URI);
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// }
-
-//Using .env file for MongoDB connection string
+Using hardcoded MongoDB connection string (not recommended for production)
 export const connectDB = async ()=> {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI); // Use the environment variable for MongoDB URI
+        const MONGO_URI = 'mongodb://127.0.0.1:27017/llcdb'; // Replace with your MongoDB connection string
+        const conn = await mongoose.connect(MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 }
+
+//Using .env file for MongoDB connection string
+// export const connectDB = async ()=> {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI); // Use the environment variable for MongoDB URI
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.error(`Error: ${error.message}`);
+//         process.exit(1);
+//     }
+// }
